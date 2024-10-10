b6a391d48a0d1547a211038d175b7ef22c428553
QueenMae <maena.brice@epitech.digital>
2024-10-10 09:19:49 +0200

    ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,10 +1,5 @@
 var now = new Date();
 var hours = now.getHours();
 var minutes = now.getMinutes();
-
-if (minutes < 10) {
-    minutes = '0' + minutes;
-}
-
 var message = "L'heure actuelle est " + hours + ":" + minutes + ".";
 document.getElementById('timeDisplay').innerText = message;
